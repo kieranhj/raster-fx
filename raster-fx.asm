7324cc5b895eefdd288f703899dc323099d0afa7
kieranhj <kieran@kconnell.co.uk>
2018-09-11 11:04:38 +0100

    Revist ye olde LED billboard demo
    But implement using Vertical Rupture

--- raster-fx.asm
+++ raster-fx.asm
@@ -59,6 +59,12 @@ TimerValue = 32*64 - 2*64 - 2 - 20
 \\ XX us to fire the timer before the start of the scanline so first colour set on column -1
 \\ YY us for code that executes after timer interupt fires
 
+BILLB_NUM_ROWS = 16
+BILLB_NUM_COLS = 80
+BILLB_ROW_BYTES = 40 * 2 * 16	; 40 columns * 2 rows * 16 bytes per char = 1280b
+
+STAR_ROW_ADDR = screen_addr - BILLB_ROW_BYTES		; row '-1'
+
 \ ******************************************************************
 \ *	ZERO PAGE
 \ ******************************************************************
@@ -73,7 +79,22 @@ GUARD &9F
 
 \\ FX variables
 
-.fx_colour_index		SKIP 1		; index into our colour palette
+.readptr				SKIP 2
+.writeptr				SKIP 2
+
+.billb_offset			SKIP 1		; 0-39
+
+.billb_left_off			SKIP 1		; = offset - 1
+.billb_right_off		SKIP 1		; = offset + 40
+.billb_mask				SKIP 1
+
+.billb_message_ptr		SKIP 2
+.billb_glyph			SKIP 1
+.billb_glyph_col		SKIP 1
+.billb_glyph_byte		SKIP 1
+
+.billb_row_count		SKIP 1
+.billb_top_row			SKIP 1
 
 \ ******************************************************************
 \ *	CODE START
@@ -125,7 +146,7 @@ GUARD screen_addr			; ensure code size doesn't hit start of screen memory
 
 	\\ Set Colour 2 to White - MODE 1 requires 4x writes to ULA Palette Register
 
-	LDA #MODE1_COL2 + PAL_white
+	LDA #MODE1_COL2 + PAL_blue
 	STA &FE21
 	EOR #&10: STA &FE21
 	EOR #&40: STA &FE21
@@ -323,11 +344,24 @@ GUARD screen_addr			; ensure code size doesn't hit start of screen memory
 
 .fx_init_function
 {
-    \ Ask OSFILE to load our screen
-	LDX #LO(osfile_params)
-	LDY #HI(osfile_params)
-	LDA #&FF
-    JSR osfile
+	FOR n,0,40,1
+	{
+		LDA #&11
+		STA STAR_ROW_ADDR + RND(BILLB_ROW_BYTES)
+	}
+	NEXT
+
+	LDA #0
+	STA billb_offset
+	STA billb_top_row
+
+	LDA #7
+	STA billb_glyph_col
+
+	LDA #LO(message_text)
+	STA billb_message_ptr
+	LDA #HI(message_text)
+	STA billb_message_ptr+1
 
 	RTS
 }
@@ -337,7 +371,7 @@ GUARD screen_addr			; ensure code size doesn't hit start of screen memory
 \
 \ The update function is used to update / tick any variables used
 \ in the FX. It may also prepare part of the screen buffer before
-\ drawing commenses but note the strict timing constraints!
+\ drawing commences but note the strict timing constraints!
 \
 \ This function will be called during vblank, after any system
 \ modules have been polled.
@@ -349,10 +383,169 @@ GUARD screen_addr			; ensure code size doesn't hit start of screen memory
 
 .fx_update_function
 {
-	\\ Increment our index into the palette table
-	INC fx_colour_index
+	LDY billb_glyph_col
+	INY
+	CPY #8
+	BCC col_ok
+
+	\\ Next char
+	LDY #0
+	.try_again
+	LDA (billb_message_ptr), Y
+	BNE char_ok
+
+	LDA #LO(message_text)
+	STA billb_message_ptr
+	LDA #HI(message_text)
+	STA billb_message_ptr+1
+	BNE try_again
+
+	.char_ok
+	INC billb_message_ptr
+	BNE no_carry
+	INC billb_message_ptr+1
+	.no_carry
+
+	SEC
+	SBC #32
+	STA billb_glyph
+
+	.col_ok
+	STY billb_glyph_col
+
+	\\ Get byte for column of glyph - don't need to calc this every time
+
+	LDA #0
+	STA readptr+1
+
+	LDA billb_glyph
+	STA readptr
+
+	ASL readptr
+	ROL readptr+1
+	ASL readptr
+	ROL readptr+1
+	ASL readptr
+	ROL readptr+1
+
+	CLC
+	LDA readptr
+	ADC #LO(glyph_data)
+	STA readptr
+	LDA readptr+1
+	ADC #HI(glyph_data)
+	STA readptr+1
+
+	\\ Get glyph byte
+
+	LDA (readptr), Y
+	STA billb_glyph_byte
+
+	\\ Scroll row left...
+
+	LDX billb_offset
+	STX billb_left_off
+
+	INX
+	CPX #BILLB_NUM_COLS/2
+	BCC offset_ok
+	LDX #0
+	.offset_ok
+	STX billb_offset
+
+	\\ Calc right offset
+
+	TXA
+	CLC
+	ADC #BILLB_NUM_COLS/2
+	STA billb_right_off
+
+	\\ Which bit?
+
+	LDA #1
+	STA billb_mask
+
+	\\ Set CRTC address for row 0
+
+	LDX billb_top_row
+	JSR billb_set_crtc_addr
+
+	\\ Draw row 0
+
+	LDY billb_right_off			; 3c
+	JSR billb_set_writeptr
+
+	LDA billb_glyph_byte
+	AND billb_mask
+	JSR billb_plot_led
+
+	LDY billb_left_off			; 3c
+	JSR billb_set_writeptr
+
+	LDA billb_glyph_byte
+	AND billb_mask
+	JSR billb_plot_led
+
 	RTS
 }
+
+.billb_set_crtc_addr		; row X
+{
+	LDA #13:STA &FE00			; 6c screen HI
+
+	CLC							; 2c
+	LDA billb_offset			; 3c
+	ASL A						; 2c
+	ADC billb_crtc_base_LO, X	; 4c
+	STA &FE01					; 4c
+
+	LDA #12:STA &FE00			; 6c screen HI
+
+	LDA billb_crtc_base_HI, X	; 4c
+	ADC #0						; 2c
+	STA &FE01					; 4c
+	RTS							; 12c
+}	; 49c
+
+.billb_set_writeptr			; row X
+{
+	CLC							; 2c
+	LDA mult16_LO, Y			; 4c
+	ADC billb_scr_base_LO, X	; 4c
+	STA writeptr				; 3c
+
+	LDA mult16_HI, Y			; 4c
+	ADC billb_scr_base_HI, X	; 4c
+	STA writeptr+1				; 3c
+	RTS							; 12c
+}	; 39c
+
+.billb_plot_led					; 6c
+{
+	\\ Needs to be fixed cost (ideally)
+	CMP #0						; 2c
+	BEQ is_zero
+	; 2c
+	LDA #LO(billb_onsprite)		; 2c
+	BEQ continue				; 3c
+
+	.is_zero					; 3c
+	LDA #LO(billb_offsprite)	; 2c
+	NOP
+
+	.continue
+	STA loop+1					; 4c
+
+	LDY #15						; 2c
+	.loop
+	LDA billb_offsprite, Y		; 4c
+	STA (writeptr), Y			; 6c
+	DEY							; 2c
+	BPL loop					; 15*3c + 2c
+
+	RTS							; 6c
+}
+\\ 74c
 
 \ ******************************************************************
 \ Draw FX
@@ -372,54 +565,160 @@ GUARD screen_addr			; ensure code size doesn't hit start of screen memory
 
 .fx_draw_function
 {
-	LDX #0					; 2c
-	LDY fx_colour_index		; 3c
+	\\ Raster is displaying row 0
 
-	\\ Shift timing so palette change happens during hblank as much as possible
+	\\ CRTC setup for rupture
 
-	FOR n,1,33,1
-	NOP
-	NEXT
-	\\ Wait 66c
+	LDA #4:STA &FE00			; vertical total
+	LDA #0:STA &FE01			; R4 = 1 - 1 = 0
+
+	LDA #6:STA &FE00			; vertical displayed
+	LDA #1:STA &FE01			; R6 = 1
+
+	LDA #7:STA &FE00			; vsync
+	LDA #&FF:STA &FE01			; R7 = &FF (no vsync)
+
+	\\ 6x 6c = 36c
+
+	\\ Set address row 1
+
+	LDX billb_top_row
+	INX
+	JSR billb_set_crtc_addr		; 49c
+
+	\\ 38c
+
+	\\ Draw row 1
+
+	ROL billb_mask	; next row
+
+	LDY billb_right_off			; 3c
+	JSR billb_set_writeptr		; 39c
+
+	LDA billb_glyph_byte				; 3c
+	AND billb_mask
+	JSR billb_plot_led			; 74c
+
+	LDY billb_left_off			; 3c
+	JSR billb_set_writeptr
+
+	LDA billb_glyph_byte
+	AND billb_mask
+	JSR billb_plot_led
+
+	\\ Draw row 1
+
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+
+	\\ REMOVE 45 NOPS
+
+	\\ Loop row rows 2-15
+
+	LDA #BILLB_NUM_ROWS - 2
+	STA billb_row_count
 
 	.raster_loop
 
-	\\ Set Colour 1 - MODE 1 requires 4x writes to ULA Palette Register
+	INX
+	TXA
+	AND #BILLB_NUM_ROWS-1
+	TAX
 
-	LDA fx_colour1_table, Y	; 4c
-	STA &FE21				; 4c
-	EOR #&10				; 2c
-	STA &FE21				; 4c
-	EOR #&40				; 2c
-	STA &FE21				; 4c
-	EOR #&10				; 2c
-	STA &FE21				; 4c
-	\\ Total = 26c
+	\\ Raster is displaying row N = X-1
 
-	\\ Set Colour 2 - MODE 1 requires 4x writes to ULA Palette Register
+	\\ Set address row N+1 = X
 
-	LDA fx_colour2_table, Y	; 4c
-	STA &FE21				; 4c
-	EOR #&10				; 2c
-	STA &FE21				; 4c
-	EOR #&40				; 2c
-	STA &FE21				; 4c
-	EOR #&10				; 2c
-	STA &FE21				; 4c
-	\\ Total = 26c
+	JSR billb_set_crtc_addr
 
-	FOR n,1,33,1
+	\\ 36c
+
+	ROL billb_mask	; next row
+
+	\\ Draw row N
+
+	LDY billb_right_off			; 3c
+	JSR billb_set_writeptr		; 39c
+
+	LDA billb_glyph_byte				; 3c
+	AND billb_mask
+	JSR billb_plot_led			; 65c
+
+	LDY billb_left_off			; 3c
+	JSR billb_set_writeptr
+
+	LDA billb_glyph_byte				; 3c
+	AND billb_mask
+	JSR billb_plot_led			; 65c
+
+	\\ 103c
+
+	FOR n,1,37,1
 	NOP
 	NEXT
-	BIT 0
-	\ Wait 69c
 
-	\\ Loop 256 times
+	JSR cycles_wait_128
+	JSR cycles_wait_128
 
-	INY						; 2c
-	INX						; 2c
+	DEC billb_row_count		; 5c
 	BNE raster_loop			; 3c
-	\\ Loop total = 26 + 26 + 69 + 7 = 128c
+
+	\\ Displaying row 15 (counting from 0)
+
+	LDA #13:STA &FE00			; screen LO
+
+	LDA #LO(STAR_ROW_ADDR/8)
+	STA &FE01
+
+	LDA #12:STA &FE00			; screen HI
+	LDA #HI(STAR_ROW_ADDR/8)
+	STA &FE01
+
+	FOR n,1,45,1
+	NOP
+	NEXT
+
+	\\ Wait row 15 - update stars?
+
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+
+	LDX #15
+	.star_loop
+
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+	JSR cycles_wait_128
+
+	DEX
+	BNE star_loop
+
+	\\ Very start of row 31, i.e. displayed 31 so far
+	\\ Need 39 total so 8 left to go
+
+	\\ Reset for vsync
+
+	LDA #4:STA &FE00			; vertical total
+	LDA #7:STA &FE01			; R4 = 7 - 1 = 6
+
+	LDA #6:STA &FE00			; vertical displayed
+	LDA #1:STA &FE01			; R6 = 1
+
+	LDA #7:STA &FE00			; vsync
+	LDA #4:STA &FE01			; R7 = &FF (no vsync)
+
+	\\ Cross fingers
 
     RTS
 }
@@ -485,76 +784,68 @@ GUARD screen_addr			; ensure code size doesn't hit start of screen memory
 	EQUB LO(screen_addr/8)	; R13 screen start address, low
 }
 
-.osfile_filename
-EQUS "Screen", 13
-
-.osfile_params
-.osfile_nameaddr
-EQUW osfile_filename
-; file load address
-.osfile_loadaddr
-EQUD screen_addr
-; file exec address
-.osfile_execaddr
-EQUD 0
-; start address or length
-.osfile_length
-EQUD 0
-; end address of attributes
-.osfile_endaddr
-EQUD 0
-
 \ ******************************************************************
 \ *	FX DATA
 \ ******************************************************************
 
-ALIGN &100
-.fx_colour1_table
+ALIGN &100				; ensure new page boundary
+.billb_onsprite
 {
-	FOR n,1,43,1
-	EQUB MODE1_COL1 + PAL_red
-	NEXT
-	FOR n,1,42,1
-	EQUB MODE1_COL1 + PAL_magenta
-	NEXT
-	FOR n,1,43,1
-	EQUB MODE1_COL1 + PAL_blue
-	NEXT
-	FOR n,1,43,1
-	EQUB MODE1_COL1 + PAL_cyan
-	NEXT
-	FOR n,1,43,1
-	EQUB MODE1_COL1 + PAL_green
-	NEXT
-	FOR n,1,42,1
-	EQUB MODE1_COL1 + PAL_yellow
+	EQUB &03, &37, &6F, &4F, &0F, &07, &03, &00			; &FF, &FF, &FF, &FF, &FF, &FF, &FF, &FF
+	EQUB &08, &0C, &0E, &0E, &0E, &0C, &08, &00			; &FF, &FF, &FF, &FF, &FF, &FF, &FF, &FF	
+}
+
+.billb_offsprite
+{
+	EQUB &20, &50, &A0, &50, &A0, &50, &20, &00			; &05, &0A, &05, &0A, &05, &0A, &05, &0A
+	EQUB &80, &40, &A0, &40, &A0, &40, &80, &00			; &05, &0A, &05, &0A, &05, &0A, &05, &0A
+}
+
+.billb_crtc_base_LO
+{
+	FOR n,0,BILLB_NUM_ROWS-1,1
+	EQUB LO((screen_addr + n * BILLB_ROW_BYTES)/8)
 	NEXT
 }
 
-.fx_colour2_table
+.billb_crtc_base_HI
 {
-	FOR n,1,21,1
-	EQUB MODE1_COL3 + PAL_red
-	NEXT
-	FOR n,1,42,1
-	EQUB MODE1_COL3 + PAL_magenta
-	NEXT
-	FOR n,1,43,1
-	EQUB MODE1_COL3 + PAL_blue
-	NEXT
-	FOR n,1,43,1
-	EQUB MODE1_COL3 + PAL_cyan
-	NEXT
-	FOR n,1,43,1
-	EQUB MODE1_COL3 + PAL_green
-	NEXT
-	FOR n,1,42,1
-	EQUB MODE1_COL3 + PAL_yellow
-	NEXT
-	FOR n,1,22,1
-	EQUB MODE1_COL3 + PAL_red
+	FOR n,0,BILLB_NUM_ROWS-1,1
+	EQUB HI((screen_addr + n * BILLB_ROW_BYTES)/8)
 	NEXT
 }
+
+.billb_scr_base_LO
+{
+	FOR n,0,BILLB_NUM_ROWS-1,1
+	EQUB LO(screen_addr + n * BILLB_ROW_BYTES)
+	NEXT
+}
+
+.billb_scr_base_HI
+{
+	FOR n,0,BILLB_NUM_ROWS-1,1
+	EQUB HI(screen_addr + n * BILLB_ROW_BYTES)
+	NEXT
+}
+
+ALIGN &100
+.mult16_LO
+FOR n,0,79,1
+EQUB LO(n * 16)
+NEXT
+
+.mult16_HI
+FOR n,0,79,1
+EQUB HI(n * 16)
+NEXT
+
+.message_text
+EQUS "HELLO WORLD!     ",0
+
+ALIGN &100
+.glyph_data
+INCBIN "square_font_90_deg_cw.bin"
 
 .data_end
 
